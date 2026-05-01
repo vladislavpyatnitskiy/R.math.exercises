b7899b552296e4ebc4744a6f99c578cd04de3b54
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-05-01 12:19:26 +0500

    Improve letter counting logic in letter_calculator
    Refactor letter_calculator to enhance efficiency and readability by streamlining letter counting logic.

--- Letter Counter.R
+++ Letter Counter.R
@@ -1,35 +1,8 @@
 letter_calculator <- function(x){ # Calculate number of each letter in word
   
-  W <- NULL
+  W <- NULL # Calculate letters for each word
   
-  for (j in 1:length(x)){ # Calculate letters for each word
-    
-    y <- tolower(x[j]) # De-capitalise all letters
-    
-    l <- NULL # Split letters
-    
-    for (m in 1:nchar(y)) l <- c(l, strsplit(y, split = "")[[1]][m])
-    
-    d <- unique(l) # Assign new variable to contain unique letters
-    
-    k <- NULL # Calculate frequency of each letter in the word
-    
-    for (m in 1:length(d)){ # For each unique letter calculate its number 
-      
-      f <- 0 # Calculate number of certain letters in word
-      
-      for (i in 1:length(l)){ # Calculate number of certain letter in a word
-        
-        f <- c(
-          sum(f, ifelse((d[m] %in% l[i]) == T, 1, 0))
-          ) 
-        
-      } 
-      
-      k <- c(k, f) # Add number for each letter
-    }
-    
-    names(k) <- d # Assign letter
+  for (j in 1:length(x)){ k <- table(strsplit(tolower(x[j]), "")[[1]])
     
     if (is.null(W)) W <- list(k) else W[[j]] <- k # Add set of number to list
   }
